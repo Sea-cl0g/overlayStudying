c863b227223912a0f46f8e16a71ba087dad778cd
boomanKnow_laptop <ukyo_shishido482@icloud.com>
2024-10-09 14:03:50 +0900

    style: 型が変更されたノード名Panel_r~aを変更後の型であるLineEdit_r~aに変更した

--- color_editor.gd
+++ color_editor.gd
@@ -6,10 +6,10 @@ extends Control
 @onready var b_slider = $VBoxContainer/HBoxContainer3/Hslider_b
 @onready var a_slider = $VBoxContainer/HBoxContainer4/HSlider_a
 
-@onready var r_panel = $VBoxContainer/HBoxContainer/Panel_r
-@onready var g_panel = $VBoxContainer/HBoxContainer2/Panel_g
-@onready var b_panel = $VBoxContainer/HBoxContainer3/Panel_b
-@onready var a_panel = $VBoxContainer/HBoxContainer4/Panel_a
+@onready var r_lineedit = $VBoxContainer/HBoxContainer/LineEdit_r
+@onready var g_lineedit = $VBoxContainer/HBoxContainer2/LineEdit_g
+@onready var b_lineedit = $VBoxContainer/HBoxContainer3/LineEdit_b
+@onready var a_lineedit = $VBoxContainer/HBoxContainer4/LineEdit_a
 
 @onready var preview = $VBoxContainer/Panel
 @onready var sheet = $"../ColorRect"
@@ -23,10 +23,10 @@ func _ready():
 	g_slider.value = sheet_col.b
 	a_slider.value = sheet_col.a
 	
-	r_panel.text = str(sheet_col.r)
-	g_panel.text = str(sheet_col.g)
-	b_panel.text = str(sheet_col.b)
-	a_panel.text = str(sheet_col.a)
+	r_lineedit.text = str(sheet_col.r)
+	g_lineedit.text = str(sheet_col.g)
+	b_lineedit.text = str(sheet_col.b)
+	a_lineedit.text = str(sheet_col.a)
 	
 	set_col(sheet_col.r, sheet_col.g, sheet_col.b, sheet_col.a)
 
@@ -34,44 +34,44 @@ func _ready():
 # R
 func _on_hslider_r_value_changed(value):
 	sheet_col.r = r_slider.value
-	r_panel.text = str(sheet_col.r)
+	r_lineedit.text = str(sheet_col.r)
 	set_col(sheet_col.r, sheet_col.g, sheet_col.b, sheet_col.a)
 
-func _on_panel_r_text_changed(new_text):
-	sheet_col.r = r_panel.text.to_int()
+func _on_lineedit_r_text_changed(new_text):
+	sheet_col.r = r_lineedit.text.to_int()
 	r_slider.value = sheet_col.r
 	set_col(sheet_col.r, sheet_col.g, sheet_col.b, sheet_col.a)
 
 # G
 func _on_hslider_g_value_changed(value):
 	sheet_col.g = g_slider.value
-	g_panel.text = str(sheet_col.g)
+	g_lineedit.text = str(sheet_col.g)
 	set_col(sheet_col.r, sheet_col.g, sheet_col.b, sheet_col.a)
 
-func _on_panel_g_text_changed(new_text):
-	sheet_col.g = g_panel.text.to_int()
+func _on_lineedit_g_text_changed(new_text):
+	sheet_col.g = g_lineedit.text.to_int()
 	g_slider.value = sheet_col.g
 	set_col(sheet_col.r, sheet_col.g, sheet_col.b, sheet_col.a)
 
 # B
 func _on_hslider_b_value_changed(value):
 	sheet_col.b = b_slider.value
-	b_panel.text = str(sheet_col.b)
+	b_lineedit.text = str(sheet_col.b)
 	set_col(sheet_col.r, sheet_col.g, sheet_col.b, sheet_col.a)
 
-func _on_panel_b_text_changed(new_text):
-	sheet_col.b = b_panel.text.to_int()
+func _on_lineedit_b_text_changed(new_text):
+	sheet_col.b = b_lineedit.text.to_int()
 	b_slider.value = sheet_col.b
 	set_col(sheet_col.r, sheet_col.g, sheet_col.b, sheet_col.a)
 
 # A
 func _on_h_slider_a_value_changed(value):
 	sheet_col.a = a_slider.value
-	a_panel.text = str(sheet_col.a)
+	a_lineedit.text = str(sheet_col.a)
 	set_col(sheet_col.r, sheet_col.g, sheet_col.b, sheet_col.a)
 
-func _on_panel_a_text_changed(new_text):
-	sheet_col.a = a_panel.text.to_int()
+func _on_lineedit_a_text_changed(new_text):
+	sheet_col.a = a_lineedit.text.to_int()
 	a_slider.value = sheet_col.a
 	set_col(sheet_col.r, sheet_col.g, sheet_col.b, sheet_col.a)
 
